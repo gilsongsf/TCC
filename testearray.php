b56d6eaf6ce2372264ede244aa9841080d797dea
Gilson Goncalves <gilson.goncalves35@hotmail.com>
2017-11-27 19:47:01 -0300

    removendo arquivos desnecessários

--- testearray.php
+++
@@ -1,6 +0,0 @@
-<script type="text/javascript">
-	var jsontext = '{"nome": "Gilson Gonçalves da Silva Filho", "id_locador": "6"}';  
-	var contact = JSON.parse(jsontext);  
-	document.write(contact.nome);  
-
-</script>
